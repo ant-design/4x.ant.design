7ce627f454a25dee8491eb7fe36e2fdf5efa2105
afc163 <afc163@users.noreply.github.com>
2024-03-19 15:55:51 +0000

    deploy: ant-design/ant-design@7c060247516925f48175d7f29cd43b626632665e

---
+++ docs/spec/research-workbench.en-US.md.6ae28b.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[309],{4172:function(e,t){e.exports={content:["article"],meta:{category:"Design Patterns (Research)",type:"Template Document",order:2,title:"Workbench",skip:!0,filename:"docs/spec/research-workbench.en-US.md"},toc:["ul"]}}}]);
